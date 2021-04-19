ade806491fef8d84ce26f9577676c1153e7ee3ae
Bogish <ego.cogitare@gmail.com>
2021-04-19 11:50:14 +0300

    ...

--- app/Jobs/TripletLogJob.php
+++ app/Jobs/TripletLogJob.php
@@ -71,7 +71,7 @@ class TripletLogJob implements ShouldQueue
         /** Save found triplet information */
         Model::create([
             'triplet' => $this->payload['triplet'],
-            'time_start' => $this->payload['time_start'] ?? 0,
+            'time_start' => $this->payload['time_start'] ?? time() * 1000,
             'time_delay' => $this->payload['time_delay'] ?? 0,
             'stock_id' => $this->payload['stock_id'],
             'profit' => $this->payload['profit'],
